9dccd2f37affa4f7615d2b511d4c6f645da54a45
aitemrar-younes <aitemrar.younes@gmail.com>
2024-01-14 16:15:17 +0100

    implemented the form hook and its good

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import '/src/assets/Styles/Login.scss'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const validationSchema = yup.object({
+    username:yup.string().required('username est obligatoire').min(3),
+    password:yup.string().required('password est obligatoire'),
+})
+const Custom_Input = ({label, register, errors, type='text', placeholder=''}) =>{
+    return (
+        <div className="input-group">
+            <input {...register(label)} className={errors[label] ? 'invalid': ''} type={type} placeholder={placeholder}/>
+            {errors && errors[label] && <span className="text-danger">{errors[label].message}</span>}
+        </div>
+    )
+}
 const Login = () => {
+    const {register, handleSubmit, formState:{errors}} = useForm({
+        resolver: yupResolver(validationSchema),
+        defaultValues: {
+            username:'',
+            password:'',
+        }
+    })
+    const onSubmit = data =>{
+        console.log(data)
+    }
   return (
     <div className='__Login__'>
         <div className="card login_card">
@@ -8,14 +34,9 @@ const Login = () => {
                 <h2>Login Card</h2>
             </div>
             <div className="card_body">
-                <form action="">
-                    <div className="input-group">
-                        <input className='invalid' type="text" placeholder='username'/>
-                        <span className="text-danger">Is required</span>
-                    </div>
-                    <div className="input-group">
-                        <input type="password" placeholder='password'/>
-                    </div>
+                <form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
+                    <Custom_Input label='username' placeholder='username' register={register} errors={errors} />
+                    <Custom_Input label='password' placeholder='password' register={register} errors={errors} type='password'/>
                     <button type="submit">Se connecter</button>
                 </form>
             </div>
