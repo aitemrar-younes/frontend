d9bfd1a544cfbfcda7be599e1f673da2cf6b439b
aitemrar-younes <aitemrar.younes@gmail.com>
2024-01-22 15:46:51 +0100

    yup date validation grather than and test method

--- src/validationSchema/ValidationSchema.jsx
+++ src/validationSchema/ValidationSchema.jsx
@@ -11,6 +11,19 @@ export const validationSchema = yup.object({
 export const accountCreateValidationSchema = yup.object({
     first_name : yup.string().required('"First name" est obligatoire'),
     last_name : yup.string().required('"Last name" est obligatoire'),
-    birth_date : yup.date().required('"Birthdate" est obligatoire'),
-    licence_expiry_date : yup.date().required('"Licence expiry date" est obligatoire'),
+    birth_date : yup.date().required('"Birthdate" est obligatoire')
+    .max(new Date, '"Birth date" ne peut pas etre dans le future')
+    .test(
+        'is-over-18',
+        'Age doit etre 18 ans minimum',
+        function (value) {
+          const currentDate = new Date();
+          const selectedDate = new Date(value);
+          const ageDifference = currentDate.getFullYear() - selectedDate.getFullYear();
+          return ageDifference >= 18;
+        }
+      ),
+    licence_expiry_date : yup.date()
+    .min(new Date, '"Licence expiry date" ne peut pas etre dans le passé')
+    .required('"Licence expiry date" est obligatoire'),
 })
